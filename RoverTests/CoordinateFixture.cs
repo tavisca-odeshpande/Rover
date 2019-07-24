4d9b9a048a9ce47503964cb709a941878d76ce6f
odeshpande <odeshpande@RENTLAPT0109>
2019-07-24 18:27:05 +0530

    Basic coordinate test

---
+++ RoverTests/CoordinateFixture.cs
@@ -0,0 +1,17 @@
+using Rover;
+using System;
+using Xunit;
+
+namespace RoverTests
+{
+    public class CoordinateFixture
+    {
+        [Fact]
+        public void can_store_coordinates()
+        {
+            Coordinate c = new Coordinate() { X = 1, Y = 2 };
+            Assert.Equal(1, c.X);
+            Assert.Equal(2, c.Y);
+        }
+    }
+}
